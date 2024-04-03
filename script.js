1f74eba35eadcae81644b46fe3bf3df7f4c59f57
Sunnat888/Sunnat888 <smuhammadiev52@gmail.com>
2024-04-03 15:42:34 +0300

    rezume

---
+++ script.js
@@ -0,0 +1,44 @@
+let elBtn = document.querySelector(".btn");
+let elForm = document.querySelector(".form");
+
+
+elBtn.addEventListener("click", () => {
+     elForm.classList.toggle("d-none")
+})
+
+
+
+let elBtp = document.querySelector(".btp");
+let elForm2 = document.querySelector(".form2");
+
+
+elBtp.addEventListener("click",  ()  =>  {
+     elForm2.classList.toggle("b-none")
+})
+
+
+let elBbp = document.querySelector(".bbp");
+let elForm3 = document.querySelector(".form3");
+
+
+elBbp.addEventListener("click",  ()  =>  {
+     elForm3.classList.toggle("h-none")
+})
+
+
+
+let elBty = document.querySelector(".bty");
+let elForm4 = document.querySelector(".form4");
+
+elBty.addEventListener("click",  ()  =>  {
+     elForm4.classList.toggle("b-none")
+})
+
+
+let elBtz = document.querySelector(".btz");
+let elForm5 = document.querySelector(".form5");
+
+
+elBtz.addEventListener("click", ()  => {
+     elForm5.classList.toggle("b-none")
+})
